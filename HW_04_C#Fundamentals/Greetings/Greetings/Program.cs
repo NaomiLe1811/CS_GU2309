5dc955449cad1e15d5f21eaa8e35e8f8bf8cdf7d
Naomi Le <thaonguyenle1811@gmail.com>
2023-10-09 17:13:18 +0300

    Edited
    Edited

---
+++ HW_04_C#Fundamentals/Greetings/Greetings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Hi! What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+
+            Console.ReadKey();
+        }
+    }
+}

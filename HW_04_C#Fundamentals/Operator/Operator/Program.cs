98edd36f23cd4795a98f3fcaa2ba6dce5436be1c
Naomi Le <thaonguyenle1811@gmail.com>
2023-10-09 17:09:32 +0300

    Edited
    Edited

---
+++ HW_04_C#Fundamentals/Operator/Operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width;
+            int height;
+            int area;
+
+            Console.WriteLine("Please input the width: ");
+            width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input the height: ");
+            height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area of this retangle is: ");
+            area = width * height;
+            Console.WriteLine(area);
+
+            Console.ReadKey();
+        }
+    }
+}

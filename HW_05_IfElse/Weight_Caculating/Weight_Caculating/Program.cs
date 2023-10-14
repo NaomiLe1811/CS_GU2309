79d1d12695a6a462791fed12d4b18553b09e8bab
Naomi Le <thaonguyenle1811@gmail.com>
2023-10-14 13:17:15 +0300

    Edited
    Edited

---
+++ HW_05_IfElse/Weight_Caculating/Weight_Caculating/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Weight_Caculating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double weight;
+            double bmi;
+
+            Console.WriteLine("Please input your hegiht: ");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please input your wegiht: ");
+            weight = float.Parse(Console.ReadLine());
+
+            bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+
+            Console.Write("Your BMI: " + bmi);
+
+            if (bmi < 18)
+                Console.WriteLine("You are underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine("You are normal");
+            else if (bmi < 30.0)
+                Console.WriteLine("You are cverweight");
+            else
+                Console.WriteLine("You are obese");
+
+            Console.ReadKey();
+        }
+    }
+}

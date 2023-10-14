ec17414c5b6c5614138c914288286325b25f2a49
Naomi Le <thaonguyenle1811@gmail.com>
2023-10-14 12:46:23 +0300

    Edited
    Edited

---
+++ HW_05_IfElse/Simple_Equation/Simple_Equation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Simple_Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            float b;
+           // int count = 3;
+
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+
+            tudau:
+            Console.WriteLine("Input a: ");
+            if (float.TryParse(Console.ReadLine(), out a)){
+                Console.WriteLine("Input b: ");
+                if (float.TryParse(Console.ReadLine(), out b)){
+                    if (a != 0)
+                    {
+                        float result = -b / a;
+                        Console.WriteLine("The result is: " + result);
+                    }
+                    else
+                    {
+                        if (b == 0)
+                        {
+                            Console.WriteLine("The result is all x ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nod soluiton");
+                        }
+                    }
+                } else
+                {
+                        Console.WriteLine("Invalid input b");
+                        goto tudau;
+                }
+            } else
+            {
+                //count--;
+                //if (count == 0)
+                //{
+                    Console.WriteLine("Invalid input a");
+                    goto tudau;
+                }
+           // }
+            Console.ReadKey();
+        }
+    }
+}

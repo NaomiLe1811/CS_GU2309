bb96b6215c4b3ba240cf70ee15217ec28a7bdae1
Naomi Le <thaonguyenle1811@gmail.com>
2023-10-14 17:13:27 +0300

    Edited
    Edited

--- HW_05_IfElse/Number_Spell_Out/Number_Spell_Out/Program.cs
+++ HW_05_IfElse/Number_Spell_Out/Number_Spell_Out/Program.cs
@@ -46,22 +46,48 @@ namespace Number_Spell_Out
                     case 17: Console.WriteLine("seven" + ones); break;
                     case 18: Console.WriteLine("eight" + ones); break;
                     case 19: Console.WriteLine("nine" + ones); break;
-                    case 20: Console.WriteLine("tw"); break;
+                    case 20: Console.WriteLine("twenty"); break;
                     default: Console.WriteLine("Out of abilitiy"); break;
 
                 }
-            } else if (laso && input >= 21 && input <= 100)
+            } else if (laso && input >= 21 && input <= 99)
             {
+                int hangchuc = input / 10;
+                int hangdonvi = input % 10;
                 
+                switch (hangchuc)
+                {
+                    case 30: Console.WriteLine("thirty"); break;
+                    case 40: Console.WriteLine("fourty"); break;
+                    case 50: Console.WriteLine("fifhty"); break;
+                    case 60: Console.WriteLine("sixty"); break;
+                    case 70: Console.WriteLine("seventy"); break;
+                    case 80: Console.WriteLine("eighty"); break;
+                    case 90: Console.WriteLine("ninety"); break;
+                    //default: Console.WriteLine("Out of abilitiy"); break;
+                }
+                switch (hangdonvi)
+                {
+                    case 1: Console.WriteLine("one"); break;
+                    case 2: Console.WriteLine("two"); break;
+                    case 3: Console.WriteLine("three"); break;
+                    case 4: Console.WriteLine("four"); break;
+                    case 5: Console.WriteLine("five"); break;
+                    case 6: Console.WriteLine("six"); break;
+                    case 7: Console.WriteLine("seven"); break;
+                    case 8: Console.WriteLine("eight"); break;
+                    case 9: Console.WriteLine("nine"); break;
+                    //default: Console.WriteLine("Out of abilitiy"); break;
+                }
+                Console.WriteLine(hangchuc * 10 + hangdonvi * 10);
             }
 
             Console.ReadKey();
         }
-        void Hangchuc(int so30, int so40, int so50, string hangchuc)
-        {
-            
-        }
 
+       
+
+        //inprocess
 
 
     }

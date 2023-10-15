f6c60c49f15428abf22cdf9e2e8ce7e3b8dc6ba1
Naomi Le <thaonguyenle1811@gmail.com>
2023-10-15 20:41:07 +0300

    Edited
    Edited

--- HW_07_Loop(1)/Prime_Number/Prime_Number/Program.cs
+++ HW_07_Loop(1)/Prime_Number/Prime_Number/Program.cs
@@ -6,7 +6,37 @@ namespace Prime_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number;
+            Console.WriteLine("Enter a number: ");
+
+            if (int.TryParse(Console.ReadLine(), out number) && number < 2)
+            {
+                Console.WriteLine(number + " is not a prime numnber");
+            }
+            else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    Console.WriteLine(number + " is a prime");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is not a prime");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }

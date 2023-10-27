1a38ad0a390259d2cb3ad449e86480d3dae4275d
Naomi Le <thaonguyenle1811@gmail.com>
2023-10-27 23:23:42 +0300

    Edited
    Edited

---
+++ HW_10_2DArray/HW_10_2DArray/HW_10_2DArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW_10_2DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many rows u want?");
+            int input = int.Parse(Console.ReadLine());
+
+            int[,] myArray = new int[,] { };
+
+            // Populate the array
+            for (int i = 0; i <= input; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(i, j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+            123      123
+            456      234
+            789      567
+
+            111
+            222
+            333
+
+        }
+    }
+}

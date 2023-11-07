c4f52c58f21ba6c5a88cbe0a6bcda84e99fc8439
Naomi Le <thaonguyenle1811@gmail.com>
2023-11-07 09:38:12 +0200

    Edited
    Edited

--- Project1_Snake_Game/Project1_Snake_Game/Program.cs
+++ Project1_Snake_Game/Project1_Snake_Game/Program.cs
@@ -1,156 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace Project1_Snake_Game
+namespace CGO_Buoi15_DuAn1
 {
-    class Program
+    class TestSnake
     {
-        ConsoleKeyInfo keypress = new ConsoleKeyInfo();
+        #region parameter
         public Random rand = new Random();
-        const int height = 20;
-        const int width = 40;
-        string dir, pre_dir;
-        //const int panel = 10;
-        public int score, headX, headY, fruitX, fruitY, typeFruit, nTail;
-        int[] TailX = new int[100]; 
+        public ConsoleKeyInfo keypress = new ConsoleKeyInfo();
+        int score, headX, headY, fruitX, fruitY, nTail;
+        int[] TailX = new int[100];
         int[] TailY = new int[100];
+        const int height = 20;
+        const int width = 60;
+        const int panel = 10;
         bool gameOver, reset, isprinted, horizontal, vertical;
-        List<(int, int)> snake; // List to store the coordinates of the snake's body segments
+        string dir, pre_dir;
+        #endregion
 
-
-        static void Main(string[] args)
+        //Hien thi man hinh bat dau
+        void ShowBanner()
         {
-            Program program = new Program();
-            program.ShowBanner();
-            while (true)
-            { 
-            program.SetUp();
-            program.Update();
-            Console.Clear();
-            }
-
-        }
-
-        public void ShowBanner()
-        {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            ConsoleKeyInfo keypress;
             //Console.SetWindowSize(width, height + panel);
-            //Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.CursorVisible = false;      //ẩn con trỏ
 
             Console.WriteLine("============================================");
             Console.WriteLine("=================SNAKE GAME=================");
             Console.WriteLine("============================================");
             Console.WriteLine();
-            Console.WriteLine("Press space to play");
+            Console.WriteLine("Pres any key to play");
             Console.WriteLine("Tips: - Use Arrow button to move the snake");
             Console.WriteLine("      - Press key P to pause game         ");
             Console.WriteLine("      - Press key R to reset game         ");
             Console.WriteLine("      - Press key Q to quit game          ");
 
-            Console.WriteLine();
-
-            keypress = Console.ReadKey(); //the data type is not explicitly stated because the Console.ReadKey() method returns a ConsoleKeyInfo object.
-            if (keypress.Key == ConsoleKey.Spacebar)
-                //GameStart();
-                if (keypress.Key == ConsoleKey.Q)
-                    Environment.Exit(0);
-            if (keypress.Key == ConsoleKey.P)
-                Console.ReadLine(); // Wait for the player to press Enter to resume
-            if (keypress.Key == ConsoleKey.R)
-            {
-                Console.Clear();
-            }
-
+            keypress = Console.ReadKey(true);
+            if (keypress.Key == ConsoleKey.Q)
+                Environment.Exit(0);
         }
-        public void SetUp()
+        //Cai dat ban dau
+        void Setup()
         {
-            dir = "RIGHT"; pre_dir = ""; //control the movement of the snake and ensure that it moves in the desired direction based on the user's input
-            score = 0;
-            gameOver = false;
-
-            // Initialize the snake with a starting length and position within the playable area
-            headX = width / 2; // Snake head X-coordinate in the center of the playable area
-            headY = height / 2; // Snake head Y-coordinate in the center of the playable area
-
+            dir = "RIGHT"; pre_dir = "";
+            score = nTail = 0;
+            gameOver = reset = isprinted = false;
+            headX = width / 2; headY = height / 2;
+            //random diem an qua
             randomQua();
-
         }
-
         //Random diem an qua
+        int typeFruit = 1;
         void randomQua()
         {
             fruitX = rand.Next(1, width - 2);
             fruitY = rand.Next(1, height - 2);
             typeFruit = rand.Next(1, 5);
         }
-
-        //In ra man hinh
-
-        void Render()
-        {
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    if (i == 0 || i == height - 1)      //vien khung
-                        Console.Write("═");
-                    else if (j == 0 || j == width - 1)  //vien khung
-                        Console.Write("║");
-                    else if (j == fruitX && i == fruitY)//in qua len man hinh
-                    {
-                        Console.Write(typeFruit);
-                    }
-                    else if (j == headX && i == headY)
-                    { //Dau con ran
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("0");
-
-                    }
-                    else
-                    {
-                        isprinted = false; //chua hieu cai nay
-                        for (int k = 0; k < nTail; k++)
-                        {
-                            if (TailX[k] == j && TailY[k] == i)
-                            {
-                                Console.Write("o");     //Than con ran
-                                isprinted = true;
-                            }
-                        }
-                        if (!isprinted) Console.Write(" ");
-                    }
-                }
-                Console.WriteLine();
-            }
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine("Your score: " + score);
-            GameStart();
-        }
-
-        void GameStart()
-        {
-            Console.WriteLine("Game started! Use arrow keys to control the snake.");
-        }
-
+        //Cap nhat man hinh
         void Update()
         {
             while (!gameOver)
             {
-            CheckInput(); //nhận lệnh điều hướng
-            Logic();      //xử lý lệnh điều hướng
-            Render();     //in ra màn hình  
-            if (reset) break;
-            Thread.Sleep(100);
+                CheckInput(); //nhận lệnh điều hướng
+                Logic();      //xử lý lệnh điều hướng
+                Render();     //in ra màn hình  
+                if (reset) break;
+                Thread.Sleep(30);
             }
 
-            if (gameOver)
-                Lose();
+            if (gameOver) Lose();
         }
-        //dieu khien phim
+        //Dieu khien phim
         void CheckInput()
         {
             while (Console.KeyAvailable)
@@ -168,7 +93,7 @@ namespace Project1_Snake_Game
                 }
             }
         }
-
+        //Kiem tra dieu huong
         void Logic()
         {
             int preX = TailX[0], preY = TailY[0];
@@ -227,6 +152,7 @@ namespace Project1_Snake_Game
                 score += typeFruit; nTail++;
                 randomQua();
             }
+
             //xac dinh chieu dang di chuyen con ran 
             if (((dir == "LEFT" && pre_dir != "UP") && (dir == "LEFT" && pre_dir != "DOWN")) || (
                 (dir == "RIGHT" && pre_dir != "UP") && (dir == "RIGHT" && pre_dir != "DOWN")))
@@ -254,6 +180,48 @@ namespace Project1_Snake_Game
                     randomQua();
             }
         }
+        //Hien thi doi tuong
+        void Render()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 0 || i == height - 1)      //vien khung
+                        Console.Write("#");
+                    else if (j == 0 || j == width - 1)  //vien khung
+                        Console.Write("#");
+                    else if (j == fruitX && i == fruitY)//qua
+                    {
+                        Console.Write(typeFruit);
+                    }
+                    else if (j == headX && i == headY)
+                    { //Dau con ran
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("0");
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                    }
+                    else
+                    {
+                        isprinted = false;
+                        for (int k = 0; k < nTail; k++)
+                        {
+                            if (TailX[k] == j && TailY[k] == i)
+                            {
+                                Console.Write("o");     //Than con ran
+                                isprinted = true;
+                            }
+                        }
+                        if (!isprinted) Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Your score: " + score);
+        }
         //Xu ly khi THUA
         void Lose()
         {
@@ -271,5 +239,16 @@ namespace Project1_Snake_Game
             }
         }
 
+        public static void Main()
+        {
+            TestSnake snake = new TestSnake();
+            snake.ShowBanner();
+            while (true)
+            {
+                snake.Setup();
+                snake.Update();
+                Console.Clear();
+            }
+        }
     }
 }
